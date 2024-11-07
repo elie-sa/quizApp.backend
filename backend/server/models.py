8fd9c4ffbb0bb7036eea6d7b8062e11e5defb453
elie-sa <e.sawmaawad@gmail.com>
2024-11-07 23:27:46 +0200

    Added models Notebook and Group

--- backend/server/models.py
+++ backend/server/models.py
@@ -7,3 +7,15 @@ class EmailConfirmationToken(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,  editable = False )
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyRel(User)
+
+class Notebook(models.Model):
+    title = models.CharField(max_length = 30)
+    color = models.CharField()
+    
+    creationDate = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null=True , on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, null = True, on_delete=models.CASCADE)
